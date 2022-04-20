f323669f164e6b16c3fa3f15cde3eb00b26571c0
Shrivtsa <68243174+shrivatsa-rao@users.noreply.github.com>
2022-04-20 10:18:21 +0530

    convert human age into dog's age

---
+++ human_year_to_dog_year.js
@@ -0,0 +1,14 @@
+var myage=22;
+//a variable named myAge, and set it equal to your age as a number.
+var earlyYears=2;
+//earlyYears and save the value 2 to it
+earlyYears *=10.5;
+
+var laterYears=myage-2;
+
+//we already accounted for the first two years, take the myAge variable, and subtract 2 from it.
+laterYears *=4;
+var myAgeInDogYears=earlyYears+laterYears;
+
+myName="shrivatsa".toLowerCase();
+console.log(`My name is ${myage}. I am ${myAgeInDogYears} years old in dog years.`);
